e34e8233b10a991183f6b10b446372b300f78466
Dave Murphy <wntrmute@users.sourceforge.net>
2005-04-11 20:27:23 +0000

    added ndstool

--- dka-scripts/build-tools.sh
+++ dka-scripts/build-tools.sh
@@ -21,6 +21,7 @@ fi
 
 g++ tools/general/bmp2bin.cpp -o $prefix/bin/bmp2bin$exeext -static -O2 -s -D__LITTLE_ENDIAN__
 gcc tools/gba/gbafix.c -o $prefix/bin/gbafix$exeext -static -O2 -s
+g++ tools/nds/ndstool.cpp -o $prefix/bin/ndstool$exeext -static -O2 -s
 cp tools/general/alignbin $prefix/bin/alignbin
 
 # Awaiting Mr_Spiv's permission to add to project
