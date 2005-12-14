45e435052def4e85c9806167bdbe01e963b2bb5e
Dave Murphy <wntrmute@users.sourceforge.net>
2005-12-14 15:29:14 +0000

    *** empty log message ***

--- dkppc/scripts/build-tools.sh
+++ dkppc/scripts/build-tools.sh
@@ -7,5 +7,7 @@ export DEVKITPRO=$TOOLPATH
 $MAKE -C tools/gamecube
 $MAKE -C tools/gamecube install PREFIX=$DEVKITPPC/bin
 
+$MAKE -C tools/general
+$MAKE -C tools/general install PREFIX=$DEVKITPPC/bin
 
 $MAKE -C tools clean
