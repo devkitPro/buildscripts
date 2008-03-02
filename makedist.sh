727cc28165b9a394b426cc16c04a52198f3b86ea
Dave Murphy <wntrmute@users.sourceforge.net>
2008-03-02 12:24:41 +0000

    use folder in distributed scripts

--- makedist.sh
+++ makedist.sh
@@ -2,4 +2,4 @@
 make -C tools clean
 DATESTRING=$(date +%Y)$(date +%m)$(date +%d)
 chmod +x build-devkit.sh
-tar --exclude=*CVS* --exclude=*.log --exclude=*.bz2 -cvjf buildscripts-$DATESTRING.tar.bz2 *
+cd .. && tar --exclude=*CVS* --exclude=*.log --exclude=*.bz2 -cvjf buildscripts-$DATESTRING.tar.bz2 buildscripts
