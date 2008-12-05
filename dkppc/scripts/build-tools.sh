5555209d7648e307dda64f716a46c5e037342698
Dave Murphy <wntrmute@users.sourceforge.net>
2008-12-05 22:45:45 +0000

    build and install wii tools

--- dkppc/scripts/build-tools.sh
+++ dkppc/scripts/build-tools.sh
@@ -7,6 +7,9 @@ export DEVKITPRO=$TOOLPATH
 $MAKE -C tools/gamecube
 $MAKE -C tools/gamecube install PREFIX=$DEVKITPPC/bin
 
+$MAKE -C tools/wii
+$MAKE -C tools/wii install PREFIX=$DEVKITPPC/bin
+
 $MAKE -C tools/general
 $MAKE -C tools/general install PREFIX=$DEVKITPPC/bin
 
