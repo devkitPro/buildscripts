bd1f2eeb4c6cd85e5fee7f5482fd5f4ebfec81e9
Dave Murphy <wntrmute@users.sourceforge.net>
2009-02-20 04:22:06 +0000

    exclude svn directories

--- makedist.sh
+++ makedist.sh
@@ -2,4 +2,4 @@
 make -C tools clean
 DATESTRING=$(date +%Y)$(date +%m)$(date +%d)
 chmod +x build-devkit.sh
-cd .. && tar --exclude=*CVS* --exclude=*.log --exclude=*.bz2 --exclude=*.gz --exclude=config.sh -cvjf buildscripts-$DATESTRING.tar.bz2 buildscripts
+cd .. && tar --exclude=*CVS* --exclude=.svn --exclude=*.log --exclude=*.bz2 --exclude=*.gz --exclude=config.sh -cvjf buildscripts-$DATESTRING.tar.bz2 buildscripts
