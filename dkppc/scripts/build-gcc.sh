a2c0d735da2a93e3bb18b16880677cbb7694385b
Dave Murphy <wntrmute@users.sourceforge.net>
2010-02-21 16:56:47 +0000

    changes for devkitPPC release 20

--- dkppc/scripts/build-gcc.sh
+++ dkppc/scripts/build-gcc.sh
@@ -101,7 +101,7 @@ then
   --with-newlib \
   --prefix=$prefix\
   --disable-dependency-tracking \
-  --with-bugurl="http://wiki.devkitpro.org/index.php/Bug_Reports" --with-pkgversion="devkitPPC release 19" \
+  --with-bugurl="http://wiki.devkitpro.org/index.php/Bug_Reports" --with-pkgversion="devkitPPC release 20" \
   2>&1 | tee gcc_configure.log
   touch configured-gcc
 fi
