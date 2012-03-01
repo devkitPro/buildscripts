f115dcaac9c6554f17bc78f82c98aa3c1ff5b5af
Dave Murphy <wntrmute@users.sourceforge.net>
2012-03-01 17:07:46 +0000

    lock svn scripts

--- build-devkit.sh
+++ build-devkit.sh
@@ -6,7 +6,7 @@
 #	devkitPSP release 16-1
 #---------------------------------------------------------------------------------
 
-if [ 0 -eq 1 ] ; then
+if [ 1 -eq 1 ] ; then
   echo "Currently in release cycle, proceed with caution, do not report problems, do not ask for support."
   echo "Please use the latest release buildscripts unless advised otherwise by devkitPro staff."
   echo "http://sourceforge.net/projects/devkitpro/files/buildscripts/"
