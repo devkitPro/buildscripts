b48a34f5a7ef09159a1b9427287b39a045ef0c91
Dave Murphy <wntrmute@users.sourceforge.net>
2012-03-31 16:59:24 +0100

    exclude .git dirs

--- makedist.sh
+++ makedist.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-make -C tools clean
 DATESTRING=$(date +%Y)$(date +%m)$(date +%d)
-chmod +x build-devkit.sh
-cd .. && tar --exclude=*CVS* --exclude=.svn --exclude=*.log --exclude=*.bz2 --exclude=*.gz --exclude=config.sh -cvjf buildscripts-$DATESTRING.tar.bz2 buildscripts
+cd .. && tar --exclude=*CVS* --exclude=.svn --exclude=.git --exclude=*.log --exclude=*.bz2 --exclude=*.gz --exclude=config.sh -cvjf buildscripts-$DATESTRING.tar.bz2 buildscripts
