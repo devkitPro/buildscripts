9f6b697806b19b36bf45df0772ad66e10d25696c
Dave Murphy <wntrmute@users.sourceforge.net>
2012-03-31 19:02:16 +0100

    ignore build directories & .gitignore file

--- makedist.sh
+++ makedist.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 DATESTRING=$(date +%Y)$(date +%m)$(date +%d)
-cd .. && tar --exclude=*CVS* --exclude=.svn --exclude=.git --exclude=*.log --exclude=*.bz2 --exclude=*.gz --exclude=config.sh -cvjf buildscripts-$DATESTRING.tar.bz2 buildscripts
+cd .. && tar	--exclude=*CVS* --exclude=.svn --exclude=.git --exclude=*.log --exclude=*.bz2 \
+				--exclude=*.gz --exclude=config.sh --exclude=.devkitARM --exclude=.devkitPPC \
+				--exclude=.devkitPSP --exclude=.gitignore \
+				-cvjf buildscripts-$DATESTRING.tar.bz2 buildscripts
