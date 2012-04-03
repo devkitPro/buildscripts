324d421723318b122ae185708ae23d21c7c612ea
Dave Murphy <wntrmute@users.sourceforge.net>
2012-04-03 14:22:24 +0100

    strip mn10200 tools for devkitPPC

--- strip_bins.sh
+++ strip_bins.sh
@@ -14,15 +14,24 @@ for f in $prefix/bin/* \
          $prefix/$target/bin/* \
          $prefix/libexec/gcc/$target/$GCC_VER/*
 do
-  # exclude dll for windows, so for linux/osx, directories .la files, embedspu script & the gccbug text file
-  if  ! [[ "$f" == *.dll || "$f" == *.so || -d $f || "$f" == *.la || "$f" == *-embedspu || "$f" == *-gccbug ]]
-  then
-    $HOST_STRIP $f
-  fi
+	# exclude dll for windows, so for linux/osx, directories .la files, embedspu script & the gccbug text file
+	if  ! [[ "$f" == *.dll || "$f" == *.so || -d $f || "$f" == *.la || "$f" == *-embedspu || "$f" == *-gccbug ]]
+	then
+		$HOST_STRIP $f
+	fi
 done
+
+if [ VERSION -eq 2 ]; then
+	for f in	$prefix/mn10200/bin/*
+	do
+		$HOST_STRIP $f
+	done
+fi
+
 
 #---------------------------------------------------------------------------------
 # strip debug info from libraries
 #---------------------------------------------------------------------------------
 find $prefix/lib/gcc/$target -name *.a -exec $target-strip -d {} \;
 find $prefix/$package/$target -name *.a -exec $target-strip -d {} \;
+
