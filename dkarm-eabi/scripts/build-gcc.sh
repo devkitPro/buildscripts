213b8b293d37760391dbf74aa5d0ca16c3de791c
Dave Murphy <davem@devkitpro.org>
2014-11-27 10:26:11 +0000

    update newlib with lock stub support

--- dkarm-eabi/scripts/build-gcc.sh
+++ dkarm-eabi/scripts/build-gcc.sh
@@ -84,12 +84,9 @@ cd $target/newlib
 
 if [ ! -f configured-newlib ]
 then
-	CFLAGS_FOR_TARGET="-DREENTRANT_SYSCALLS_PROVIDED -D__DEFAULT_UTF8__ -O2" ../../newlib-$NEWLIB_VER/configure \
+	../../newlib-$NEWLIB_VER/configure \
 	--disable-newlib-supplied-syscalls \
 	--enable-newlib-mb \
-	--enable-newlib-io-long-long \
-	--enable-newlib-io-c99-formats \
-	--enable-newlib-io-pos-args \
 	--target=$target \
 	--prefix=$prefix \
 	|| { echo "Error configuring newlib"; exit 1; }
