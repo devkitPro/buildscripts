be191f464a51c6987e5386d6064fa72a1bcfdc93
Dave Murphy <davem@devkitpro.org>
2014-11-28 21:48:03 +0000

    put 3ds crt0 only in armv6k/fpu multilib

--- dkarm-eabi/scripts/build-crtls.sh
+++ dkarm-eabi/scripts/build-crtls.sh
@@ -21,7 +21,7 @@ $MAKE CRT=gp32_gpsdk
 $MAKE CRT=ds_arm7
 $MAKE CRT=ds_arm9
 $MAKE CRT=ds_cart
-$MAKE CRT=3dsx
+$target-gcc -march=armv6k -mfloat-abi=hard -c 3dsx_crt0.s -o armv6k/fpu/3dsx_crt0.o
 
 cd $BUILDDIR/libgba-$LIBGBA_VER
 $MAKE || { echo "error building libgba"; exit 1; }
