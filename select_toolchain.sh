6698343534f20492b1e4346b0ec763fc722ca9c5
Dave Murphy <davem@devkitpro.org>
2015-02-01 23:37:05 +0000

    upgrade to gdb 4.8.2

--- select_toolchain.sh
+++ select_toolchain.sh
@@ -43,7 +43,7 @@ case "$VERSION" in
     GCC_VER=4.9.2
     BINUTILS_VER=2.25
     NEWLIB_VER=2.0.0
-    GDB_VER=7.7
+    GDB_VER=7.8.2
     basedir='dkppc'
     package=devkitPPC
     target=powerpc-eabi
