329d6149509065e517bfe6c9a346a688fc48f3b1
Dave Murphy <davem@devkitpro.org>
2015-02-02 00:59:13 +0000

    update mingw cross location

--- cross-build-i686-w64-mingw32.sh
+++ cross-build-i686-w64-mingw32.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export CROSSBUILD=i686-w64-mingw32
-export CROSSLIBPATH=/opt/i686-w64-mingw32/mingw/lib
-export CROSSBINPATH=/opt/i686-w64-mingw32/mingw/bin
-export PATH=/opt/i686-w64-mingw32/bin:$PATH
+export CROSSLIBPATH=/opt/mingw32/mingw/lib
+export CROSSBINPATH=/opt/mingw32/mingw/bin
+export PATH=/opt/mingw32/bin:$PATH
 export PKG_CONFIG_PATH=$CROSSLIBPATH/pkgconfig
 
