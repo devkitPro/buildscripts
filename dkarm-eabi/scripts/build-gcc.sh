88e9abb8a0fe098af14f644e467126f51d9b2340
Dave Murphy <davem@devkitpro.org>
2016-01-26 15:13:25 +0000

    update devkitARM version

--- dkarm-eabi/scripts/build-gcc.sh
+++ dkarm-eabi/scripts/build-gcc.sh
@@ -56,7 +56,7 @@ then
 		--with-headers=../../newlib-$NEWLIB_VER/newlib/libc/include \
 		--prefix=$prefix \
 		--enable-lto $plugin_ld\
-		--with-bugurl="http://wiki.devkitpro.org/index.php/Bug_Reports" --with-pkgversion="devkitARM release 45" \
+		--with-bugurl="http://wiki.devkitpro.org/index.php/Bug_Reports" --with-pkgversion="devkitARM release 46" \
 		$CROSS_PARAMS \
 		|| { echo "Error configuring gcc"; exit 1; }
 	touch configured-gcc
