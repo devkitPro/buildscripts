0f6e0d6db964f32734140289f6f815d1e0da98c1
Dave Murphy <davem@devkitpro.org>
2016-01-31 01:57:06 +0000

    keep host libs out of cross build

--- cross-build-i686-w64-mingw32.sh
+++ cross-build-i686-w64-mingw32.sh
@@ -3,4 +3,5 @@ export CROSSBUILD=i686-w64-mingw32
 export CROSSLIBPATH=/opt/mingw32/mingw/lib
 export CROSSBINPATH=/opt/mingw32/mingw/bin
 export PATH=/opt/mingw32/bin:$PATH
-export PKG_CONFIG_PATH=$CROSSLIBPATH/pkgconfig
+export PKG_CONFIG_PATH=
+export PKG_CONFIG_LIBDIR=$CROSSLIBPATH/pkgconfig
