b0f2701d3092befc7fb07bda8992c6996a38c4f9
Dave Murphy <davem@devkitpro.org>
2017-03-04 22:51:11 +0000

    update x86_64 cross

--- cross-build-x86_64-w64-mingw32.sh
+++ cross-build-x86_64-w64-mingw32.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 export CROSSBUILD=x86_64-w64-mingw32
-export CROSSLIBPATH=/opt/x86_64-mingw-w64/mingw/lib
-export CROSSBINPATH=/opt/x86_64-mingw-w64/mingw/bin
-export PATH=/opt/x86_64-mingw-w64/bin:$PATH
-export PKG_CONFIG_PATH=$CROSSLIBPATH/pkgconfig
+export CROSSLIBPATH=/opt/mingw64/mingw/lib
+export CROSSBINPATH=/opt/mingw64/mingw/bin
+export PATH=/opt/mingw64/bin:$PATH
+export PKG_CONFIG_PATH=
+export PKG_CONFIG_LIBDIR=$CROSSLIBPATH/pkgconfig
 
