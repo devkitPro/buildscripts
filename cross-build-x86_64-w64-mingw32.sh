50478190585c79b0794f9d7a0a0b69ed5a49e2a9
Dave Murphy <davem@devkitpro.org>
2017-06-27 19:49:13 +0100

    split common part of path

--- cross-build-x86_64-w64-mingw32.sh
+++ cross-build-x86_64-w64-mingw32.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 export CROSSBUILD=x86_64-w64-mingw32
-export CROSSLIBPATH=/opt/mingw64/mingw/lib
-export CROSSBINPATH=/opt/mingw64/mingw/bin
+export CROSSPATH=/opt/mingw64/mingw
+export CROSSLIBPATH=$CROSSPATH/lib
+export CROSSBINPATH=$CROSSPATH/bin
 export PATH=/opt/mingw64/bin:$PATH
 export PKG_CONFIG_PATH=
 export PKG_CONFIG_LIBDIR=$CROSSLIBPATH/pkgconfig
