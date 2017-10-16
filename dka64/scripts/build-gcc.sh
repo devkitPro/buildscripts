5e5c9fea8780c6029c4bb7de23c668a39cd48abe
Dave Murphy <davem@devkitpro.org>
2017-10-16 14:47:28 +0100

    update version & bug url

--- dka64/scripts/build-gcc.sh
+++ dka64/scripts/build-gcc.sh
@@ -62,7 +62,7 @@ then
 		--prefix=$prefix \
 		--enable-lto $plugin_ld\
 		--with-system-zlib \
-		--with-bugurl="http://wiki.devkitpro.org/index.php/Bug_Reports" --with-pkgversion="devkitA64 alpha 4" \
+		--with-bugurl="https://github.com/devkitPro/buildscripts/issues" --with-pkgversion="devkitA64 alpha 5" \
 		$CROSS_PARAMS \
 		|| { echo "Error configuring gcc"; exit 1; }
 	touch configured-gcc
