46d25cdbb96a31a9457da331d494150c58807b69
Dave Murphy <davem@devkitpro.org>
2018-02-11 14:27:27 +0000

    ignore all toolchain build folders

--- makedist.sh
+++ makedist.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 DATESTRING=$(date +%Y)$(date +%m)$(date +%d)
 cd .. && tar	--exclude=*CVS* --exclude=.svn --exclude=.git --exclude=*.log --exclude=*.bz2 \
-				--exclude=*.gz --exclude=config.sh --exclude=.devkitARM --exclude=.devkitPPC \
-				--exclude=.devkitPSP --exclude=.gitignore \
+				--exclude=*.gz --exclude=config.sh --exclude=.devkitARM* --exclude=.devkitPPC* \
+				--exclude=.devkitA64* --exclude=.gitignore \
 				-cvjf buildscripts-$DATESTRING.tar.bz2 buildscripts
