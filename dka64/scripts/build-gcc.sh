caa95f24567d819a85a0bcd3f5705df72a5ee0fd
Dave Murphy <davem@devkitpro.org>
2018-03-26 15:30:02 +0100

    let's call this a release

--- dka64/scripts/build-gcc.sh
+++ dka64/scripts/build-gcc.sh
@@ -63,7 +63,7 @@ then
 		--prefix=$prefix \
 		--enable-lto $plugin_ld\
 		--with-system-zlib \
-		--with-bugurl="https://github.com/devkitPro/buildscripts/issues" --with-pkgversion="devkitA64 alpha 8" \
+		--with-bugurl="https://github.com/devkitPro/buildscripts/issues" --with-pkgversion="devkitA64 release 8" \
 		$CROSS_PARAMS \
 		|| { echo "Error configuring gcc"; exit 1; }
 	touch configured-gcc
