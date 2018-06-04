27b541afae3d7fe0b590bb05528a43b7bc27cc16
Dave Murphy <davem@devkitpro.org>
2018-06-04 15:35:55 +0000

    enable std::filesystem

--- dkppc/scripts/build-gcc.sh
+++ dkppc/scripts/build-gcc.sh
@@ -90,6 +90,7 @@ then
 	--disable-libstdcxx-pch \
 	--disable-libstdcxx-verbose \
 	--enable-libstdcxx-time=yes \
+	--enable-libstdcxx-filesystem-ts \
 	--enable-cxx-flags='-ffunction-sections -fdata-sections' \
 	--target=$target \
 	--with-newlib \
