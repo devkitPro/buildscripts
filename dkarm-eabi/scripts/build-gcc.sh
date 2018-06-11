0c2e648a04f4e1c6757be7a9c9e8b587a1828df2
Dave Murphy <davem@devkitpro.org>
2018-06-11 13:27:09 +0100

    add lto to languages

--- dkarm-eabi/scripts/build-gcc.sh
+++ dkarm-eabi/scripts/build-gcc.sh
@@ -46,7 +46,7 @@ then
 	CXXFLAGS_FOR_TARGET="-O2 -ffunction-sections -fdata-sections" \
 	LDFLAGS_FOR_TARGET="" \
 	../../gcc-$GCC_VER/configure \
-		--enable-languages=c,c++ \
+		--enable-languages=c,c++,lto \
 		--with-gnu-as --with-gnu-ld --with-gcc \
 		--with-march=armv4t\
 		--enable-cxx-flags='-ffunction-sections' \
