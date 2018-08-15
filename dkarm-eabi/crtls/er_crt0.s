da8d9c793b449c66fc281ef045d72d2d920b652e
Dave Murphy <davem@devkitpro.org>
2018-08-15 02:02:33 +0100

    explicitly MPL crt0 files. Closes #31

--- dkarm-eabi/crtls/er_crt0.s
+++ dkarm-eabi/crtls/er_crt0.s
@@ -4,6 +4,10 @@
 @ author  : tim schuerewegen
 @ version : 1.0
 @---------------------------------------------------------------------------------
+@	This Source Code Form is subject to the terms of the Mozilla Public License,
+@	v. 2.0. If a copy of the MPL was not distributed with this file, You can
+@	obtain one at https://mozilla.org/MPL/2.0/.
+@---------------------------------------------------------------------------------
 	.section	".crt0","ax"
 	.global     _start
 	.align
