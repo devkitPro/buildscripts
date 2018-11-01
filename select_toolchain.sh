0c9af4df257100434d1a216a6dc4bfabb677806b
Dave Murphy <davem@devkitpro.org>
2018-11-01 13:12:44 +0000

    use binutils 2.31.1

--- select_toolchain.sh
+++ select_toolchain.sh
@@ -40,8 +40,8 @@ case "$VERSION" in
     toolchain=DEVKITARM
   ;;
   "2" )
-    BINUTILS_VER=2.30
     GCC_VER=8.2.0
+    BINUTILS_VER=2.31.1
     MN_BINUTILS_VER=2.17
     NEWLIB_VER=3.0.0
     GDB_VER=8.0.1
