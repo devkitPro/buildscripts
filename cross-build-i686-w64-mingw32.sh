41b418fd8cf4f2917556da02c74fcff4f027e228
Dave Murphy <davem@devkitpro.org>
2019-01-30 08:57:46 +0000

    consolidate paths

--- cross-build-i686-w64-mingw32.sh
+++ cross-build-i686-w64-mingw32.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 export CROSSBUILD=i686-w64-mingw32
-export CROSSLIBPATH=/opt/mingw32/mingw/lib
-export CROSSBINPATH=/opt/mingw32/mingw/bin
+export CROSSPATH=/opt/mingw32/mingw
+export CROSSLIBPATH=$CROSSPATH/lib
+export CROSSBINPATH=$CROSSPATH/bin
 export PATH=/opt/mingw32/bin:$PATH
+
