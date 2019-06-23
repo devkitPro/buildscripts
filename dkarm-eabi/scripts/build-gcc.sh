fe202a6f50cd5df4634595e8a66d0e7aefe8c92e
Dave Murphy <davem@devkitpro.org>
2019-06-23 10:30:17 +0100

    devkitARM: update gcc to 9.1.0

--- dkarm-eabi/scripts/build-gcc.sh
+++ dkarm-eabi/scripts/build-gcc.sh
@@ -64,7 +64,7 @@ then
 		--prefix=$prefix \
 		--enable-lto\
 		--with-system-zlib \
-		--with-bugurl="http://wiki.devkitpro.org/index.php/Bug_Reports" --with-pkgversion="devkitARM release 52-2" \
+		--with-bugurl="http://wiki.devkitpro.org/index.php/Bug_Reports" --with-pkgversion="devkitARM release 53" \
 		$CROSS_PARAMS \
 		$CROSS_GCC_PARAMS \
 		|| { echo "Error configuring gcc"; exit 1; }
