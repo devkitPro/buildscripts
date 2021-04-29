771225f45a9d774b2fac57b5a9d78e32dadea091
Dave Murphy <davem@devkitpro.org>
2021-04-29 18:16:23 +0100

    devkitPPC: move mn10200 binutils to 2.24

--- select_toolchain.sh
+++ select_toolchain.sh
@@ -41,7 +41,7 @@ case "$VERSION" in
   "2" )
     GCC_VER=11.1.0
     BINUTILS_VER=2.36.1
-    MN_BINUTILS_VER=2.17
+    MN_BINUTILS_VER=2.24
     NEWLIB_VER=4.1.0
     basedir='dkppc'
     package=devkitPPC
