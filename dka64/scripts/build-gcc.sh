9ae621ad6203a432f337964555c97649055fc1d2
Dave Murphy <davem@devkitpro.org>
2021-08-01 02:15:48 +0100

    devkitA64: upgrade gcc to 11.2

--- dka64/scripts/build-gcc.sh
+++ dka64/scripts/build-gcc.sh
@@ -65,7 +65,7 @@ then
 		--enable-lto \
 		--disable-tm-clone-registry \
 		--disable-__cxa_atexit \
-		--with-bugurl="https://github.com/devkitPro/buildscripts/issues" --with-pkgversion="devkitA64 release 17" \
+		--with-bugurl="https://github.com/devkitPro/buildscripts/issues" --with-pkgversion="devkitA64 release 18" \
 		$CROSS_PARAMS \
 		$CROSS_GCC_PARAMS \
 		$EXTRA_GCC_PARAMS \
