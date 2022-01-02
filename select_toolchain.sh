3ca5189cddf5d070c2f35dd27b15f2812205d5fb
Dave Murphy <davem@devkitpro.org>
2022-01-02 18:19:39 +0000

    devkitA64: update newlib to 4.2.0.20211231

--- select_toolchain.sh
+++ select_toolchain.sh
@@ -51,7 +51,7 @@ case "$VERSION" in
   "3" )
     GCC_VER=11.2.0
     BINUTILS_VER=2.32
-    NEWLIB_VER=4.1.0
+    NEWLIB_VER=4.2.0.20211231
     basedir='dka64'
     package=devkitA64
     target=aarch64-none-elf
