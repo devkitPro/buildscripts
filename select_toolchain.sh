73b1cb9d7a73a5afc8781e74c774aa2b276f0636
Dave Murphy <davem@devkitpro.org>
2022-01-03 12:19:10 +0000

    devkitPPC: update newlib to 4.2.0.20211231

--- select_toolchain.sh
+++ select_toolchain.sh
@@ -42,7 +42,7 @@ case "$VERSION" in
     GCC_VER=11.2.0
     BINUTILS_VER=2.37
     MN_BINUTILS_VER=2.24
-    NEWLIB_VER=4.1.0
+    NEWLIB_VER=4.2.0.20211231
     basedir='dkppc'
     package=devkitPPC
     target=powerpc-eabi
