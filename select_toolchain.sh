a370be5c4af4ca51230cdd047fc16ec808212dff
Dave Murphy <davem@devkitpro.org>
2022-05-02 22:19:23 +0100

    devkitA64: update gcc to 12.1.0-RC-20220429

--- select_toolchain.sh
+++ select_toolchain.sh
@@ -49,7 +49,7 @@ case "$VERSION" in
     toolchain=DEVKITPPC
   ;;
   "3" )
-    GCC_VER=11.2.0
+    GCC_VER=12.1.0-RC-20220429
     BINUTILS_VER=2.32
     NEWLIB_VER=4.2.0.20211231
     basedir='dka64'
